416266c7b9fb21ce83bf953db6c037e1a1873f2f
levinoi <levi.alya@gmail.com>
2025-09-02 23:09:00 +0500

    hometask from Lesson28

--- src/test/java/com/herokuapp/tests/stepDefinitions/LoginSteps.java
+++ src/test/java/com/herokuapp/tests/stepDefinitions/LoginSteps.java
@@ -8,8 +8,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.herokuapp.core.BasePage.driver;
 
